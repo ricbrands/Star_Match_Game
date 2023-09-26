1c6dd387792cdeff17bc92e5e63cf6e05917967d
Ricardo Brandao <rpvbrandao@gmail.com>
2023-09-26 01:07:16 -0300

    feat: first commit with the basic project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import StartMatch from './StarMatch';
+
+function App() {
+  return (
+    <div className="App">
+      <StartMatch />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/StarMatch.jsx
@@ -0,0 +1,62 @@
+export default function StarMatch() {
+    return (
+        <div className="game">
+            <div className="help">
+                Pick 1 or more numbers that sum to the number of stars
+            </div>
+            <div className="body">
+                <div className="left">
+                    <div className="star" />
+                    <div className="star" />
+                    <div className="star" />
+                    <div className="star" />
+                    <div className="star" />
+                    <div className="star" />
+                    <div className="star" />
+                    <div className="star" />
+                    <div className="star" />
+                </div>
+                <div className="right">
+                    <button className="number">1</button>
+                    <button className="number">2</button>
+                    <button className="number">3</button>
+                    <button className="number">4</button>
+                    <button className="number">5</button>
+                    <button className="number">6</button>
+                    <button className="number">7</button>
+                    <button className="number">8</button>
+                    <button className="number">9</button>
+                </div>
+            </div>
+            <div className="timer">Time Remaining: 10</div>
+        </div>
+    );
+};
+
+const color = {
+    available: 'lightgray',
+    used: 'lightgreen',
+    wrong: 'lightcoral',
+    candidate: 'deepskyblue',
+};
+
+const utils = {
+    sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
+    range: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
+    random: (min, max) => min + Math.floor.floor(Math.random() * (max - min + 1)),
+    randomSumIn: (arr, max) => {
+        const sets = [[]];
+        const sums = [];
+        for (let i=0; i<arr.length; i++){
+            for (let j=0, len=sets.length; j<len; j++) {
+                const candidateSet = sets[j].concat(arr[i]);
+                const candidateSum = utils.sum(candidateSet);
+                if (candidateSum <= max) {
+                    sets.push(candidateSet);
+                    sums.push(candidateSum);
+                }
+            }
+        }
+        return sums[utils.random(0, sums.length - 1)];
+    },
+};
